bd252711675a948899a3a4c1cc4c5c9ef71b23fa
Adrian Knapp <Aknapp1212@gmail.com>
2016-02-14 02:00:05 -0500

    Added block creation file

---
+++ Swiftris/Block.swift
@@ -0,0 +1,11 @@
+//
+//  Block.swift
+//  Swiftris
+//
+//  Created by Adrian Knapp on 2/14/16.
+//  Copyright © 2016 PIGStudio. All rights reserved.
+//
+
+import SpriteKit
+
+let NumberOfColors: UInt32 = 6
