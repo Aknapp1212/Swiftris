a1f0eb66500031be0dac1d28a28a85a492a94780
Adrian Knapp <Aknapp1212@gmail.com>
2016-02-15 23:39:03 -0500

    Set up the rotation shapes

--- Swiftris/Block.swift
+++ Swiftris/Block.swift
@@ -9,3 +9,37 @@
 import SpriteKit
 
 let NumberOfColors: UInt32 = 6
+
+enum BlockColor: Int, CustomStringConvertible {
+    
+    // #3
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    // #4
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    // #5
+    var description: String {
+        return self.spriteName
+    }
+    
+    // #6
+    static func random() -> BlockColor {
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
